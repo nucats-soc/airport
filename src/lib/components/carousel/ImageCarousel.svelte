20e75a813704b44d122571e4bd2de360be38cb67
amnexya <jack@eilles.xyz>
2026-07-27 19:41:55 +0100

    chore: create ImageCarousel.svelte and add basic functions

---
+++ src/lib/components/carousel/ImageCarousel.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    
+    let { images } = $props();
+    let current = $state(0);
+
+    function nextImg() {
+      current = (current + 1) % images.length;
+    }
+
+    function prevImg() {
+        current = (current - 1 + images.length) % images.length;
+    }
+
+    // auto image scrolling, every 3000ms
+    onMount(() => {
+      const interval = setInterval(nextImg, 3000);
+      return () => clearInterval(interval);
+    });
+
+</script>
+

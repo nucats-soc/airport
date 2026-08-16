054c874d642bf3e7bf7e4dd7e08c18f208aedb51
amnexya <jack@eilles.xyz>
2026-08-16 22:03:36 +0100

    feat: Create ImageCarousel and add to Home page (#3)
    * chore: create ImageCarousel.svelte and add basic functions
    
    * chore: add images for carousel
    
    * feat: finish ImageCarousel.svelte
    
    * feat: add ImageCarousel to homepage
    
    * feat: add Carousel animation to layout
    
    * feat: finish image carousel, add accessibility
    
    * fix: edit styles on carousel buttons
    
    * chore: remove IconButton.svelte, not needed
    
    * fix: small fix to linkbutton to specify no icon
    
    * Remove Footer component from +page.svelte
    
    Removed Footer component from the main page layout.

---
+++ src/lib/components/carousel/ImageCarousel.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import Button from '$lib/components/button/ActionButton.svelte';
+	import carouselData from '$lib/assets/carousel/AltText.json';
+
+	// Load images from assets
+	const imageFiles = import.meta.glob('/src/lib/assets/carousel/*.{jpg,png,jpeg}', {
+		eager: true,
+		import: 'default'
+	});
+
+	const images = carouselData.map((image) => ({
+		src: imageFiles[`/src/lib/assets/carousel/${image.src}`],
+		alt: image.alt
+	}));
+
+	let carousel: HTMLDivElement;
+
+	// Manual carousel scroll
+	function scrollCarousel(direction: number) {
+		carousel.scrollBy({
+			left: direction * 350,
+			behavior: 'smooth'
+		});
+	}
+</script>
+
+<div class="relative">
+	<!-- Conditional if reduce motion is enabled -->
+	<div class="absolute top-1/2 left-4 z-10 -translate-y-1/2">
+		<Button type="icon" onClick={() => scrollCarousel(-1)} extraClass="carousel-button">←</Button>
+	</div>
+
+	<div class="absolute top-1/2 right-4 z-10 -translate-y-1/2">
+		<Button type="icon" onClick={() => scrollCarousel(1)} extraClass="carousel-button">→</Button>
+	</div>
+
+	<div
+		bind:this={carousel}
+		class="inline-flex w-full scrollbar-none flex-nowrap overflow-x-auto mask-[linear-gradient(to_right,transparent_0,black_128px,black_calc(100%-200px),transparent_100%)] pb-12"
+	>
+		<ul class="infinite-scroll flex items-center justify-center md:justify-start">
+			{#each images as image}
+				<li class="mx-4">
+					<img
+						class="max-h-64 max-w-none rounded-[1.25rem]"
+						src={image.src}
+						alt={image.alt}
+						loading="lazy"
+					/>
+				</li>
+			{/each}
+		</ul>
+
+		<ul
+			class="infinite-scroll flex items-center justify-center md:justify-start"
+			aria-hidden="true"
+			role="presentation"
+		>
+			{#each images as image}
+				<li class="mx-4">
+					<img
+						class="max-h-64 max-w-none rounded-[1.25rem]"
+						src={image.src}
+						alt=""
+						loading="lazy"
+					/>
+				</li>
+			{/each}
+		</ul>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script>
-    import Hero from "$lib/sections/Hero.svelte";
-    import Socials from "$lib/sections/Socials.svelte";
-    import About from "$lib/sections/About.svelte";
-    import EventsOverview from "$lib/sections/EventsOverview.svelte";
-    import Footer from "$lib/components/navigation/Footer.svelte";
+	import Hero from '$lib/sections/Hero.svelte';
+	import Socials from '$lib/sections/Socials.svelte';
+	import About from '$lib/sections/About.svelte';
+	import EventsOverview from '$lib/sections/EventsOverview.svelte';
+	import Footer from '$lib/components/navigation/Footer.svelte';
+	import ImageCarousel from '$lib/components/carousel/ImageCarousel.svelte';
 </script>
 
-<Hero/>
-<About/>
-<Socials/>
-<EventsOverview/>
+<Hero />
+<ImageCarousel />
+<About />
+<Socials />
+<EventsOverview />

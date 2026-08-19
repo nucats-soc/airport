11f5d91c579f020fba65e739b61d7e62b53d7e92
amnexya <jack@eilles.xyz>
2026-08-19 23:51:13 +0100

    fix: revert card gaps

--- src/routes/committee/+page.svelte
+++ src/routes/committee/+page.svelte
@@ -25,11 +25,11 @@
 	</AnimatedCatBackground>
 
 	<Container>
-		<div class="flex flex-col gap-8">
+		<div class="flex flex-col gap-4">
 			{#if data.committeeMembers.length === 0}
 				<p class="tx-body text-zinc-300">Committee members will be listed here soon.</p>
 			{:else}
-				<div class="grid gap-8 6 sm:grid-cols-2 lg:grid-cols-4">
+				<div class="grid gap-4 6 sm:grid-cols-2 lg:grid-cols-4">
 					{#each data.committeeMembers as member}
 						<CommitteeMemberCard {member} />
 					{/each}

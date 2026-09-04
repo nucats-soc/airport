8bce07b6defda415d27df176d20a70397ead3b49
amnexya <jack@eilles.xyz>
2026-09-04 18:38:17 +0100

    fix: remove tos and privacy components and add to main page

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,5 +1,44 @@
 <script lang="ts">
-    import PrivacyPolicy from './_components/PrivacyPolicy.svelte';
+    import Container from '$lib/components/layout/Container.svelte';
 </script>
 
-<PrivacyPolicy />
+<Container>
+    <h1 class="tx-page-title">Privacy Policy</h1>
+    <p class="tx-page-subtitle">Last updated: 2026-09-04</p>
+    <p>This is "nucats.org" ("we", "us", "our").</p>
+    <p>As a society, we are committed to protecting the privacy of our members and users.</p>
+    <p>Any additional questions about this privacy policy should be directed to soc-computingtechnology@ncl.ac.uk.</p>
+</Container>
+<br/>
+<Container>
+    <h2 class="tx-section-title">Information we collect</h2>
+    <p>When visiting our site, we will collect the following information:</p>
+    <ul class="list-disc list-inside">
+        <li>IP address</li>
+        <li>Browser type and version</li>
+        <li>Operating system</li>
+        <li>Referring website</li>
+        <li>Pages visited on our site</li>
+        <li>Date and time of visit</li>
+    </ul>
+    <p>This data is collected automatically when you browse to any page on our site.</p>
+    <p>This data is used to help with debugging and improving our site, as well as identifying users that break our terms of service.</p>
+    <br>
+    <p>The following links are for third party Privacy Policies, as we use their services on this website:</p>
+    <ul class="list-disc list-inside">
+        <li><a class="text-blue-500 hover:underline" href="https://policies.google.com/privacy?hl=en-US" target="_blank" rel="noopener noreferrer">Google for Google Maps</a></li>
+        <li><a class="text-blue-500 hover:underline" href="https://privacycenter.notion.so/policies" target="_blank" rel="noopener noreferrer">Notion for Events Fetching and Committee Information</a></li>
+    </ul>
+    <p>All data held by these third parties is removable by us, however your data is subject to the providers own policies.</p>
+    <br>
+    <h2 class="tx-section-title">Cookies</h2>
+    <p>We do not collect cookies. This section will be updated if this situation changes.</p>
+    <br>
+    <h2 class="tx-section-title">Changes to this Privacy Policy</h2>
+    <p>We may update this privacy policy from time to time. We will notify you of any changes by posting an announcement in our Discord server.</p>
+    <p>There will be a 48 hour notice period before we change the page.</p>
+    <br>
+    <h2 class="tx-section-title">Your rights to your data</h2>
+    <p>You have the right to request a copy of, or delete your personal data at any time, no questions asked.</p>
+    <p>To do this, please contact us at soc-computingtechnology@ncl.ac.uk.</p>
+</Container>

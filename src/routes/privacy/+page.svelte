d4c9926c032050a6879e3438bd115ea1b1453bd5
amnexya <jack@eilles.xyz>
2026-09-04 17:42:11 +0100

    feat: add privacy policy and tos pages

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import PrivacyPolicy from './_components/PrivacyPolicy.svelte';
+</script>
+
+<PrivacyPolicy />
---
+++ src/routes/tos/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import TermsOfService from './_components/TermsOfService.svelte';
+</script>
+
+<TermsOfService />
